86e44b38b000aedda6098f225c24b8517f15883f
RosarioGLara <rglara2@miners.utep.edu>
2025-06-29 13:25:53 -0600

    changes

---
+++ main.py
@@ -0,0 +1,8 @@
+import tkinter as tk
+
+root = tk.Tk()
+root.title("MyTimeSheet")
+root.geometry("300x200")
+label = tk.Label(root, text="Welcome to your MyTimeSheet!")
+label.pack(pady=20)
+root.mainloop()
